cf9ad713bfcfeb748a241c1b32ea35531d8e1432
TC008800 <Kevin3617@outlook.com>
2024-07-31 20:19:47 +0800

    delete conpile files

--- MDK-ARM/standard_tpye_c/pressen_asm.d
+++
@@ -1,1 +0,0 @@
-standard_tpye_c\pressen_asm.o: ..\components\presSen_asm.s
--- MDK-ARM/standard_tpye_c/spl06.d
+++
@@ -1,3 +0,0 @@
-standard_tpye_c\spl06.o: spl06.cpp
-standard_tpye_c\spl06.o: spl06.h
-standard_tpye_c\spl06.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
